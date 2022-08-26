af5f321be06c1d3b8c83436255f272ad8d3dbb4c
Abiodun <aabiodunawoyemi@gmail.com>
2022-08-26 02:50:16 +0100

    event and emit added

--- contracts/Todo.sol
+++ contracts/Todo.sol
@@ -1,21 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
-    
-contract Todo {
 
+contract Todo {
     struct Task {
         string task;
-        uint deadline;
+        uint256 deadline;
         bool isCompleted;
     }
 
     mapping(address => Task[]) todoTask;
 
-    event SetTask(address indexed user, string indexed task, uint indexed deadline);
-    event UpdateTask(address indexed user, string indexed task, uint indexed taskIndex);
-    event DeleteTask(address indexed user, uint indexed _taskIndex);
+    event SetTask(
+        address indexed user,
+        string indexed task,
+        uint256 indexed deadline
+    );
+    event UpdateTask(
+        address indexed user,
+        string indexed task,
+        uint256 indexed taskIndex
+    );
+    event DeleteTask(address indexed user, uint256 indexed _taskIndex);
 
-    function setTask(string calldata _task, uint _deadline) external {
+    function setTask(string calldata _task, uint256 _deadline) external {
         address user = msg.sender;
         require(user != address(0), "cannot set task to the zero address");
 
@@ -24,25 +31,24 @@ contract Todo {
         emit SetTask(user, _task, _deadline);
     }
 
-    function getTask() external view returns(Task[] memory) {
+    function getTask() external view returns (Task[] memory) {
         Task[] storage todos = todoTask[msg.sender];
         return todos;
     }
 
-    function updateTask(uint _taskIndex, string calldata _task) external {
+    function updateTask(uint256 _taskIndex, string calldata _task) external {
         Task storage todo = todoTask[msg.sender][_taskIndex];
         todo.task = _task;
-        
+
         emit UpdateTask(msg.sender, _task, _taskIndex);
     }
 
-    function completeTask(uint _taskIndex) external {
+    function completeTask(uint256 _taskIndex) external {
         Task storage todo = todoTask[msg.sender][_taskIndex];
         todo.isCompleted = true;
     }
 
-
-    function deleteTask(uint _taskIndex) external {
+    function deleteTask(uint256 _taskIndex) external {
         delete todoTask[msg.sender][_taskIndex];
 
         emit DeleteTask(msg.sender, _taskIndex);
